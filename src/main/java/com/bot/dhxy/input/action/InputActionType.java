9961aa1bb0daf6c42c2f9203e870418c528ebb74
岳云峰 <yueyunfe@yahoo.com>
2026-05-18 19:12:34 -0400

    Add input action model

---
+++ src/main/java/com/bot/dhxy/input/action/InputActionType.java
@@ -0,0 +1,12 @@
+package com.bot.dhxy.input.action;
+
+public enum InputActionType {
+    CLICK_LEFT,
+    CLICK_RIGHT,
+    DOUBLE_RIGHT_CLICK,
+    TYPE_TEXT_UNICODE,
+    PRESS_ENTER,
+    SCROLL_DOWN,
+    SCROLL_UP,
+    SLEEP
+}
