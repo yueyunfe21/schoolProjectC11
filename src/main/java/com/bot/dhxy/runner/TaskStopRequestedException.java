c752b0f9313380987588ca46c485719ee34643b1
岳云峰 <yueyunfe@yahoo.com>
2026-05-18 12:20:01 -0400

    Deprecate old task stop exception location

--- src/main/java/com/bot/dhxy/runner/TaskStopRequestedException.java
+++ src/main/java/com/bot/dhxy/runner/TaskStopRequestedException.java
@@ -1,11 +1,10 @@
 package com.bot.dhxy.runner;
 
 /**
- * 任务主动停止异常。
- *
- * 作用：任务内部发现停止信号后，可以用这个异常快速跳出当前流程。
+ * @deprecated use {@link com.bot.dhxy.runner.stop.TaskStopRequestedException}
  */
-public class TaskStopRequestedException extends RuntimeException {
+@Deprecated
+public class TaskStopRequestedException extends com.bot.dhxy.runner.stop.TaskStopRequestedException {
 
     public TaskStopRequestedException(String message) {
         super(message);
