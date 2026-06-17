95fe48866fa1f4057bfdad23637467aaee3c60f9
Yunfeng Yue <yueyunfe@yahoo.com>
2026-06-17 01:19:50 -0400

    Add task attention ready event type

--- src/main/java/com/bot/dhxy/window/model/WindowReadyEventType.java
+++ src/main/java/com/bot/dhxy/window/model/WindowReadyEventType.java
@@ -4,5 +4,6 @@ package com.bot.dhxy.window.model;
  * Soft wake signal types published by window-level observers.
  */
 public enum WindowReadyEventType {
-    PATHING_TERMINAL
+    PATHING_TERMINAL,
+    TASK_ATTENTION_REQUIRED
 }
